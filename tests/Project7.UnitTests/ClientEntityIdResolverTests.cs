dd422d53fade4e56f1fd7d99781b11c84aae2ce0
tasadar2 <tassadar@wowway.com>
2019-07-11 18:46:55 -0400

    sample replication

---
+++ tests/Project7.UnitTests/ClientEntityIdResolverTests.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading;
+using Xunit;
+
+namespace Project.UnitTests
+{
+    public class TestClass
+    {
+        [Fact]
+        public void Test()
+        {
+            Thread.Sleep(5000);
+        }
+    }
+}
